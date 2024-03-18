699717ad928ac3882c99df8b98e2a90bbdf02e17
BaeDongHyeon <sasd730@gmail.com>
2024-03-18 21:58:40 +0900

    Initial commit

---
+++ src/main/java/com/yeogi/triplog/TriplogApplication.java
@@ -0,0 +1,13 @@
+package com.yeogi.triplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplogApplication.class, args);
+	}
+
+}
