b4db0418de98461e4ddda3527514c1a74cbac074
BaeDongHyeon <sasd730@gmail.com>
2024-05-10 21:10:17 +0900

    feat(*): 미입력 칸이 존재하면 오류 메시지 출력 - MemberController: 검증을 통해 오류 체크 - signUpPage: 오류가 있으면 메시지 출력

--- src/main/java/com/yeogi/triplog/controller/MemberController.java
+++ src/main/java/com/yeogi/triplog/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.yeogi.triplog.controller;
 
 import com.yeogi.triplog.domain.member.form.MemberSignUpForm;
 import com.yeogi.triplog.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +24,12 @@ public class MemberController {
     }
 
     @PostMapping("/signup")
-    public String signUpMember(@ModelAttribute MemberSignUpForm memberSignUpForm) {
+    public String signUpMember(@Valid @ModelAttribute MemberSignUpForm memberSignUpForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "member/signUpPage";
+        }
+
         memberService.signUpMember(memberSignUpForm.toEntity());
         return "redirect:/";
     }
