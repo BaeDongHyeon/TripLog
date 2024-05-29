e45993b6feda2d779d92aa044b38c3dc0f97e7c8
BaeDongHyeon <sasd730@gmail.com>
2024-05-29 21:16:00 +0900

    feat(*): 가입버튼 클릭시 이메일 검증 확인 구현 - emailDuplicateCheck: 이메일을 검증했는지 확인하는 로직 구현 - signUpPage: 각 입력창에 함수 연결 및 id 추가

--- src/main/resources/static/js/emailDuplicateCheck.js
+++ src/main/resources/static/js/emailDuplicateCheck.js
@@ -1,3 +1,9 @@
+let isEmailValid = false;
+
+function resetEmailValidationStatus() {
+    isEmailValid = false;
+}
+
 $('#duplicateCheckButton').on('click', function () {
     let data = {
         email: $('#email').val()
@@ -12,8 +18,10 @@ $('#duplicateCheckButton').on('click', function () {
     }).done(function (result) {
         if (result) {
             alert('이미 존재하는 이메일입니다.')
+            isEmailValid = false;
         } else {
             alert('사용이 가능한 이메일입니다.');
+            isEmailValid = true;
         }
         // 성공
     }).fail(function (jqXHR) {
@@ -21,8 +29,25 @@ $('#duplicateCheckButton').on('click', function () {
             let errors = jqXHR.responseJSON;
             let errorMessage = errors.email;
             alert(errorMessage);
+            isEmailValid = false;
         } else {
             alert('오류가 발생하였습니다.');
+            isEmailValid = false;
         }
     });
+});
+
+// 이메일 입력 필드가 변경될 때 상태 초기화
+$('#email').on('input', function () {
+    resetEmailValidationStatus();
+});
+
+// 가입하기 버튼 클릭 시 이메일 검증 상태를 확인
+$('#signUpButton').on('click', function () {
+    if (!isEmailValid) {
+        alert('이메일 중복 확인을 완료해주세요.');
+        return false;
+    }
+
+    $('#signUpForm').submit();
 });
