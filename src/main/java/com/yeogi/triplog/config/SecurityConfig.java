09407df40cdf26c3f57593e8a3f8152a98e90f37
BaeDongHyeon <sasd730@gmail.com>
2024-06-26 21:38:58 +0900

    refactor(SecurityConfig): REST API 테스트를 위한 임시 허용

--- src/main/java/com/yeogi/triplog/config/SecurityConfig.java
+++ src/main/java/com/yeogi/triplog/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/h2-console/**", "/signup", "/login", "/css/**").permitAll()
+                        .requestMatchers("/h2-console/**", "/signup", "/login", "/css/**", "/js/**", "/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
                         .anyRequest().authenticated()
